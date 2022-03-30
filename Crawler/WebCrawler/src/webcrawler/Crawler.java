3c4507e0fdcc43063c4334a414453d614ddadc54
thawanip <thawanip@uwindsor.ca>
2022-03-30 16:54:21 -0400

    Added Text processing code
    Added Regex, Trie for stop words and comments

--- Crawler/WebCrawler/src/webcrawler/Crawler.java
+++ Crawler/WebCrawler/src/webcrawler/Crawler.java
@@ -1,42 +1,51 @@
 package webcrawler;
 
 import org.jsoup.Jsoup;
-import org.jsoup.Jsoup;   
 import org.jsoup.nodes.Document;   
 import org.jsoup.nodes.Element;   
 import org.jsoup.select.Elements;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap; 
 
+/**
+* This code is used to implement the Web Crawler using JSoup
+*
+* @author  Prasham Thawani
+* @StudentId 110065917
+*/
 public class Crawler {
 
 	private static HashMap<String, PageData> urlLinks = new HashMap<String, PageData>();
 	private static final int maximum_depth = 3;
 	
-	
+	/**
+    * Inserts string URL and the depth until which Crawler will crawl
+    * @param url string URL
+    * @param depth the depth until which crawler will crawl
+    */
 	public static void crawl(String url, int depth)
 	{
-		if(urlLinks.size() < 150 && !urlLinks.containsKey(url) && depth < maximum_depth)
+		//Max number of URLs to crawl and condition to limit depth
+		if(urlLinks.size() < 200 && !urlLinks.containsKey(url) && depth < maximum_depth)
 		{
-			try {
+			try 
+			{
+				//get document using Jsoup
 				Document page = Jsoup.connect(url).get();
 				
+				//add url and page text into the List
 				urlLinks.put(url, new PageData(depth, page.title(), page.text()));
+				//Get links on present url page
 				Elements linksOnUrl = page.select("a[href]");
 				depth++;
 				System.out.println("Crawling url: "+url);
+				//Call crawl recursively
 				for(Element link : linksOnUrl)
-				{
-					
-					crawl(link.attr("abs:href"), depth);
-				}
-				
+					crawl(link.attr("abs:href"), depth);				
 			} 
 			catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -48,6 +57,8 @@ public class Crawler {
 		 * System.out.println("Depth:"+urlLinks.get(key).depth
 		 * +" Title: "+urlLinks.get(key).title); }
 		 */
+		
+		//Write urls to a csv file
 		try (PrintWriter writer = new PrintWriter("test.csv")) {
 
 		      StringBuilder sb = new StringBuilder();
